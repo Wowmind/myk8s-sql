07ebfc6ccdbd3362d7de5e808fee06c2e4900a7a
Taiwo <Bellotaiwoa30@gmail.com>
2024-01-12 00:18:58 +0100

    private-connection

--- vpc.tf
+++ vpc.tf
@@ -37,3 +37,19 @@ resource "google_compute_subnetwork" "data" {
 
   private_ip_google_access = true
 }
+// create a private access connection to allow web subnet connection to sql
+resource "google_compute_global_address" "private-ip-peering" {
+  name          = "google-managed-services-custom"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = google_compute_network.custom.id
+}
+
+resource "google_service_networking_connection" "private-vpc-connection" {
+  network = google_compute_network.custom.id
+  service = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [
+    google_compute_global_address.private-ip-peering.name
+  ]
+}
