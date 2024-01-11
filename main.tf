200e1dd5b8f0a23d5785c9d75d6df092161b9557
Taiwo <Bellotaiwoa30@gmail.com>
2024-01-12 00:12:02 +0100

    sql

--- main.tf
+++ main.tf
@@ -46,4 +46,64 @@ resource "google_container_cluster" "private" {
   }
 }
 
-//
+// create a mysql instance
+
+resource "random_string" "db_name_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "google_sql_database_instance" "mysql" {
+
+  # Instance info
+  name             = "mysql-private-${random_string.db_name_suffix.result}"
+  region           = var.region
+  database_version = var.mysql_database_version
+
+  settings {
+
+    # Region and zonal availability
+    availability_type = var.mysql_availability_type
+    location_preference {
+      zone = var.mysql_location_preference
+    }
+
+    # Machine Type
+    tier              = var.mysql_machine_type
+
+    # Storage
+    disk_size         = var.mysql_default_disk_size
+
+    # Connections
+    ip_configuration {
+      ipv4_enabled        = false
+      private_network     = google_compute_network.custom.id
+    }
+
+    # Backups
+    backup_configuration {
+      binary_log_enabled = true
+      enabled = true
+      start_time = "06:00"
+    }
+  }
+  depends_on = [
+    google_service_networking_connection.private-vpc-connection
+  ]
+}
+
+data "google_secret_manager_secret_version" "wordpress-admin-user-password" {
+  secret = "wordpress-admin-user-password"
+}
+
+resource "google_sql_database" "wordpress" {
+  name     = "wordpress"
+  instance = google_sql_database_instance.mysql.name
+}
+
+resource "google_sql_user" "wordpress" {
+  name = "wordpress"
+  instance = google_sql_database_instance.mysql.name
+  password = data.google_secret_manager_secret_version.wordpress-admin-user-password.secret_data
+}
--- variable.tf
+++ variable.tf
@@ -22,3 +22,27 @@ variable "gke_master_ipv4_cidr_block" {
   type    = string
   default = "172.23.0.0/28"
 }
+variable "mysql_location_preference" {
+  type = string
+  default = "europe-west1-b"
+}
+
+variable "mysql_machine_type" {
+  type = string
+  default = "db-n1-standard-2"
+}
+
+variable "mysql_database_version" {
+  type = string
+  default = "MYSQL_8_0"
+}
+
+variable "mysql_default_disk_size" {
+  type = string
+  default = "100"
+}
+
+variable "mysql_availability_type" {
+  type = string
+  default = "REGIONAL"
+}
