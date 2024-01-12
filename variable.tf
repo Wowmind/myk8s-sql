5c339320328a8204ef8499ce203594721e0008c2
Taiwo <Bellotaiwoa30@gmail.com>
2024-01-12 17:38:20 +0100

    update

--- variable.tf
+++ variable.tf
@@ -15,7 +15,7 @@ variable "zone" {
 variable "authorized_source_ranges" {
   type        = list(string)
   description = "Addresses or CIDR blocks which are allowed to connect to GKE API Server."
-  default = [ "10.0.0.0/24" ]
+  default = [ "10.0.0.0/24", "172.23.0.0/28"]
 }
 
 variable "gke_master_ipv4_cidr_block" {
